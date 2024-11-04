cdc32c95ca8b9b23a24708a1d430fcec0e5af714
wallhuynh <phuctuong0862@gmail.com>
2024-11-04 18:19:11 +0700

    little change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md text-center">
-      <h2 className="text-2xl font-bold text-gray-800 mb-6">
-        Đăng ký tài khoản
-      </h2>
+      <h2 className="text-2xl font-bold text-gray-800 mb-6">Đăng nhập</h2>
 
       <form id="registrationForm" className="space-y-4">
         <div className="text-left">
